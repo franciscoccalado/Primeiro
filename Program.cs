dc9300b48545ec53ed39557645cab5a839758415
FRANCISCO CARLOS CALADO <francisco.ccalado@senacsp.edu.br>
2018-04-11 09:00:12 -0300

    declaraçao de variaveis

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace PrimeiroGit
     {
         static void Main(string[] args)
         {
-         
+         //Declaraçao das variaveis para guardar os valores
+// que o usuario entrara
+double valor1,valor2,resultado;
         }
     }
 }
+
+
