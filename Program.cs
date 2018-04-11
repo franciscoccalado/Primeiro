d5095eb666348e4a1c86a9330dbb7a7a6b0a9210
FRANCISCO CARLOS CALADO <francisco.ccalado@senacsp.edu.br>
2018-04-11 09:16:42 -0300

    Interagir co m usuario

--- Program.cs
+++ Program.cs
@@ -9,6 +9,12 @@ namespace PrimeiroGit
          //Declaraçao das variaveis para guardar os valores
 // que o usuario entrara
 double valor1,valor2,resultado;
+Console.WriteLine("Digite um valor:");
+valor1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Entre com outro valor");
+valor2 = double.Parse(Console.ReadLine());
+
         }
     }
 }
